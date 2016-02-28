e578c9e905d1fa1287f809c41c07c0ad9eb145a5
David Bebb <davidbebb@gmail.com>
2016-02-28 07:56:24 +0100

    added weekly views, daily views in progress;

--- routes/db.js
+++ routes/db.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 /* GET Userlist page. */
-router.get('/datapoint/:id', function(req, res) {
+router.get('/data/id/:id', function(req, res) {
     var db = req.db;
     var idNumber = req.params.id;
     db.list(function(err, body){
@@ -24,22 +24,81 @@ router.get('/datapoint/:id', function(req, res) {
 
 });
 
+router.get('/data/all', function(req, res) {
+    var db = req.db;
+      db.list(function(err, body) {
+        if (!err) {
+          var dataArray = [];
+          var gatewayEui =  "1DEE15E85FA7DCEF";
+          var length = body.rows.length;
+          var data = [];
+          body.rows.forEach(function(doc) {
+            var id = doc.id;
+            db.get(id,  function(err, doc) {
+                if (doc.payload.gatewayEui != gatewayEui) {
+                  length = length-1;
+                } else {
+                dataArray.push(doc.payload);
+                }
+                if (dataArray.length === length){
+                  dataArray.sort(dynamicSort("time"));
+                  dataArray.forEach(function(doc){
+                    data.push(processData(doc.data, doc.time));
+                    if (data.length === length){
+                      toSend = split(data);
+                      res.json(toSend);
+                    };
+                  });
+                };
+            });
+          });
+        };
+      });
+});
+
+function split(data) {
+  var dataArray = [];
+  var timeArray = [];
+  l = data.length;
+  for (i = 0; i < l; i++) {
+    dataArray.push(data[i][0]);
+    timeArray.push(data[i][1]);
+  }
+
+  return [dataArray, timeArray];
+}
+
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
 
 
 function processData(data, endTime) {
   res = dataToArray(data);
-  time = getTimes(endTime, res.length);
+  time =  new Date(endTime);
   return [res, time];
 };
 
 function dataToArray(data){
   var buf = new Buffer(data, 'base64').toString("ascii");
   var res = buf.split(",");
-  return res;
+  res.shift();
+  l = res.length;
+  foo = (res.reduce((a, b) => a + b, 0)/l);
+  return foo;
 };
 
 function getTimes(time, number) {
-  var diff = 15;
+  var diff = 0.1;
   endTime = new Date(time);
   timeArray = [endTime];
   for (i = 1; i < number; i++) {
